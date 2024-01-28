e1320d35bc9bc42f41be4c47ebdf5766ed0a59e2
yumiiiiiii <102947194+yumiiiiiii@users.noreply.github.com>
2024-01-28 21:19:00 +0900

    유저 기능 추가

---
+++ accounts/serializers.py
@@ -0,0 +1,97 @@
+from rest_framework import serializers
+from .models import User
+
+# class SignUpSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = User
+#         fields = ('__all__')
+#         extra_kwargs = {"password": {"write_only":True}}
+
+#     def create(self, validated_data):
+#         user = User.objects.create_user(**validated_data)
+#         user.set_password(validated_data['password'])
+#         user.save()
+
+#         return user
+
+class SignUpSerializer(serializers.Serializer):
+    class Meta:
+        model=User
+        fields=['id', 'username','password','nickname', 'email', 'gender', 'age']
+        extra_kwargs = {"password": {"write_only":True}}
+
+    gender_list = (
+        ('남', '남'),
+        ('여', '여')
+    )
+
+    username = serializers.CharField(max_length=20) # 아이디
+    password = serializers.CharField()
+    email = serializers.EmailField()
+    nickname = serializers.CharField(max_length=20) # 이름
+    gender = serializers.ChoiceField(
+        choices=gender_list
+    )
+    age = serializers.IntegerField()
+
+    def create(self, validated_data):
+
+        if User.objects.filter(username=validated_data['username']).exists() or User.objects.filter(email=validated_data['email']).exists():
+            raise serializers.ValidationError('username 존재 or email 존재')
+
+
+        else:
+            user = User.objects.create(
+                username=validated_data['username'],
+                nickname=validated_data['nickname'],
+                email=validated_data['email'],
+                gender=validated_data['gender'],
+                age=validated_data['age']
+            )
+            user.set_password(validated_data['password'])
+            user.save()
+            return user
+
+class LoginSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = User
+        fields=['id', 'username','password','nickname', 'email', 'gender', 'age']
+        extra_kwargs = {"password": {"write_only":True}}
+
+    gender_list = (
+        ('남', '남'),
+        ('여', '여')
+    )
+
+    username = serializers.CharField(max_length=20) # 아이디
+    password = serializers.CharField()
+    email = serializers.EmailField()
+    nickname = serializers.CharField(max_length=20) # 이름
+    gender = serializers.ChoiceField(
+        choices=gender_list
+    )
+    age = serializers.IntegerField()
+
+
+    def validate(self, data):
+        username = data.get("username", None)
+        password = data.get("password", None)
+
+        if User.objects.filter(username=username).exists():
+            user = User.objects.get(username=username)
+            if not user.check_password(password):
+                raise serializers.ValidationError('잘못된 비밀번호입니다.')
+        else:
+            raise serializers.ValidationError('존재하지 않는 사용자입니다.')
+        
+        data = {
+            'id': user.id,
+            'username':user.username,
+            'nickname' : user.nickname,
+            'email' : user.email,
+            'gender' : user.gender,
+            'age' : user.age,
+        }
+
+        return data
--- dislodged_project/settings/base.py
+++ dislodged_project/settings/base.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
-
+AUTH_USER_MODEL = 'accounts.User'
 
 # Application definition
 
@@ -40,6 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'rest_framework_simplejwt',
+
+    "accounts",
 ]
 
 MIDDLEWARE = [
@@ -90,6 +95,54 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+from datetime import timedelta
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True # 쿠키 허용
+
+#인증 방식
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES' : [
+         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ]
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': False,
+    'UPDATE_LAST_LOGIN': False,
+
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': SECRET_KEY,
+    'VERIFYING_KEY': None,
+    'AUDIENCE': None,
+    'ISSUER': None,
+    'JWK_URL': None,
+    'LEEWAY': 0,
+
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+    'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
+
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+    'TOKEN_USER_CLASS': 'rest_framework_simplejwt.models.TokenUser',
+
+    'JTI_CLAIM': 'jti',
+
+    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+}
+
 
 
 # Internationalization
