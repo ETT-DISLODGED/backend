3ab02b2d9edd95b2ba46f5e6439fd390dc64b767
yumiiiiiii <102947194+yumiiiiiii@users.noreply.github.com>
2024-03-25 22:26:37 +0900

    voice_info default 수정

--- accounts/models.py
+++ accounts/models.py
@@ -32,9 +32,9 @@ class User(AbstractUser):
     
 class Voice_Info(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    speed = models.FloatField(null=True, blank=True)
-    pitch = models.FloatField(null=True, blank=True)
-    type = models.CharField(max_length=100, null=True, blank=True) # textfield는 default 지정이 안됨. 게다가 Django 3.2.4 이상이어야 함...
+    speed = models.FloatField(default=1)
+    pitch = models.FloatField(default=0)
+    type = models.CharField(max_length=100, default="ko-KR-Standard-A") # textfield는 default 지정이 안됨. 게다가 Django 3.2.4 이상이어야 함...
 
     def __str__(self):
         return f'{self.user} voice_info'
